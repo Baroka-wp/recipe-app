641e92c39daaf7a532ddc7dfc4cbc44869b58f31
Jonathan Sivahera <jsivahera@icloud.com>
2022-08-29 15:15:13 +0200

    Add recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,55 @@
 class RecipesController < ApplicationController
-  def index; end
+   def index
+    @recipes = Recipe.where(user_id: current_user.id).includes(:recipe_foods)
 
-  def show; end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @recipes }
+      format.json { render :json => @recipes }
+    end
+  end
 
-  def create; end
+  def show
+    @recipe = Recipe.where(user_id: current_user.id).find(params[:id]).includes(:recipe_foods)
+  end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+    respond_to do |format|
+      format.html { render :new, locals: { recipe: @recipe } }
+    end
+  end
+
+  def create
+    # new object from params
+    @recipe = Recipe.new(post_params)
+
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        if @recipe.save
+          # success message
+          flash[:success] = 'Recipe created successfully'
+          # redirect to index
+          redirect_to recipes_url
+        else
+          flash.now[:error] = 'Error: Recipe could not be created'
+          # render new
+          render :new, locals: { recipe: @recipe }
+        end
+      end
+    end
+  end
+
+  def destroy
+    Recipe.find(params[:id]).delete
+  end
+
+  private
+
+  def post_params
+    post_params = params.require(:recipe).permit(:name, :prep_time, :cooking_time, :description, :public)
+    post_params[:user_id] = current_user.id
+    post_params
+  end
 end
