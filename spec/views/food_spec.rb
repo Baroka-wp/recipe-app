a0dffbf93ed4dbca8e43b94a126b17f5088e6718
baroka <birotori@gmail.com>
2022-08-31 10:45:49 +0100

    test : request and integration testes

--- spec/views/food_spec.rb
+++ spec/views/food_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe Food, type: :view do
     before :each do
         @user = User.create(name: "Tom", email:"tom@dev.co", password: "password", password_confirmation: "password")
         @food = Food.create(name: "Pasta", measurement_unit: "Litter", price: 10, user_id: @user.id)
-        @recipe = Recipe.create(name: "Pasta", description: "Itelian", prep_time: 10, cooking_time: 10, user_id: @user.id)
-        @recipe = Recipe.create(name: "rita", description: "french", prep_time: 10, cooking_time: 10, public: false, user_id: @user.id)
-
     end
 
     context 'Visite public recipe' do
         it 'should show public recipe' do
-            visit public_recipes_path
-            expect(page).not_to have_content('rita')
+            visit new_user_session_path
+            fill_in 'user[email]', with: @user.email
+            fill_in 'user[password]', with: @user.password
+            click_button 'Log in'
+            visit foods_path
             expect(page).to have_content('Pasta')
         end
     end
